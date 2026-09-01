9b064546e00006b63bac982ee3b28144ab002002
YelibeT <yelibe44@gmail.com>
2026-09-01 17:31:29 +0300

    Fix Vercel backend serverless setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://biniyamblog.vercel.app",
+      "https://personal-blog-jade-nine.vercel.app/",
     ],
     credentials: true,
   })
